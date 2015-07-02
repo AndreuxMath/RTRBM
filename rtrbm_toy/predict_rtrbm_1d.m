bc88cd63a0c17ac3eabb600bcfae117688730b02
Mathieu <mathieu.andreux@gmail.com>
2015-07-02 12:35:47 +0200

    train11.mat: new rtrbm parameters after a true gibbs sampling predict_rtrbm_1d.m: modified to make most probable prediction

--- rtrbm_toy/predict_rtrbm_1d.m
+++ rtrbm_toy/predict_rtrbm_1d.m
@@ -1,11 +1,20 @@
-function [vt, rt] = predict_rtrbm_1d( v1, rtrbm)
+function [vt, rt] = predict_rtrbm_1d( v1, rtrbm, mode)
 
 % given rtrbm, and initial states v1 (up to size(v1,2)
 % infer the sequence up to t=T
 % every generated testing sequence is averaged from num_iter many trails
 
-pre_gibbs_k = 10;
-gibbs_k = 20;
+% 02/07/2015: Modification by Mathieu Andreux (added 3rd input = mode)
+% mode = 1: prediction is made with the most probable sample (from gibbs)
+% mode = 2 prediction is made with the mean of the gibbs samples (default)
+% mode = 3 prediction is made with the last of the gibbs samples
+
+if(nargin <3)
+    mode =2;
+end
+
+pre_gibbs_k =5000;
+gibbs_k = 1000;
 
 %%
 vt = zeros( rtrbm.d_v, rtrbm.T);
@@ -30,14 +39,24 @@ for t = t1+1: rtrbm.T
     end
     
     vt_k = zeros( rtrbm.d_v, gibbs_k);
+    ht_k = zeros( rtrbm.d_h, gibbs_k);
     for kk = 1: gibbs_k 
         h = sigmrnd(   rtrbm.W * v + rtrbm.b + rtrbm.U * rt(:,t-1) );
         v = sigmrnd( (rtrbm.W).'*h + rtrbm.c ); 
         vt_k(:,kk) = v; 
+        ht_k(:,kk) = h;
     end
     
-    vt(:, t) = mean( vt_k, 2); % mean over the gibbs sampling path
-    %vt(:, t) = vt_k(:, gibbs_k);
+    switch mode
+        case 1
+            E = sum(ht_k.*(rtrbm.W*vt_k),1) + (rtrbm.c')*vt_k + (rtrbm.b')*ht_k + (rtrbm.U * rt(:,t-1))' * ht_k;
+            where = find(E==max(E));
+            vt(:,t) = vt_k(:,where(1));% most probable among the samples
+        case 2
+            vt(:, t) = mean( vt_k, 2); % mean over the gibbs sampling path
+        case 3
+            vt(:, t) = vt_k(:, gibbs_k); % last observed
+    end
     rt(:, t) = sigm( rtrbm.W * vt(:,t)  + rtrbm.b + rtrbm.U *rt(:, t-1));
 end
 
